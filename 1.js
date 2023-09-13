cb279c879a962ba052503310e6f34faedbaab5c6
Zofi Furnaleta <143588099+ZofiAlfi@users.noreply.github.com>
2023-09-13 20:16:42 +0700

    Update 1.js

--- 1.js
+++ 1.js
@@ -1,4 +1,6 @@
 const scrollUp = document.querySelector("#scroll-up");
+const day = ["Minggu", "Senin", "Selasa", "Rabu", "Kamis", "Jumat", "Sabtu"];
+const MONTH = ["Jan", "Feb", "Mar", "Apr", "Mei", "Jun", "Jul", "Agu", "Sep", "Okt", "Nov", "Des"]
 
 scrollUp.addEventListener("click",() =>{
 	window.scrollTo({
@@ -7,3 +9,25 @@ scrollUp.addEventListener("click",() =>{
 		behavior: "smooth",
 	});
 });
+
+function updateTime() {
+	var now = new Date();
+
+	/*document.getElementById("time").innerText =
+			zeroPadding(now.getHours(), 2) + ":" +
+			zeroPadding(now.getMinutes(), 2) + ":" +
+			zeroPadding(now.getSeconds(), 2);*/
+
+	document.getElementById("date").innerText =
+			day[now.getDay()] + ", " +
+			zeroPadding(now.getDate(), 2) + " " +
+			MONTH[now.getMonth()] + " " +	
+			now.getFullYear();
+}
+
+updateTime();
+setInterval(updateTime, 1000);
+
+function zeroPadding(num, digit) {
+	return String(num).padStart(digit, '0');
+}
